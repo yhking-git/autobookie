0b495751a36299993789a42c5c359a1b5ca09e61
YH-King <kyh10000@outlook.com>
2022-06-28 18:00:26 +0800

    v1.2.0-pre9 introduce AlgoSigner

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 const algosdk = require('algosdk');
-const prompt = require("prompt-sync")({ sigint: true });
 const fs = require('fs');
 const {stringToByteArray, numberToByteArray, stringToTimestamp, sleep} = require('./util');
 
@@ -164,7 +163,6 @@ class AutobookieCore {
   }
 
   /**
-   * 
    * @param {string} mnemonic 
    * @param {number} appId 
    */
@@ -401,12 +399,167 @@ class AutobookieCore {
 
     return suggestedParams
   }
-
 }
+
+/**
+ * Interface of AlgoSigner
+ */
+class AlgoSignerWrapper {
+  /**
+   * @param {string} ledgerName 
+   * @param {number} usdcAssetId 
+   * @param {number} fixedFee 
+   */
+  constructor(ledgerName, usdcAssetId, fixedFee) {
+    AlgoSigner.connect();
+    this.ledgerName = ledgerName;
+    this.usdcAssetId = usdcAssetId;
+    this.fixedFee = fixedFee;
+  }
+
+  /**
+  * @param {string} mnemonic 
+  * @param {number} appId 
+  */
+  async userPrepareBetting(mnemonic, appId) {
+    console.log("Prepare Betting starting...");
+    const account = algosdk.mnemonicToSecretKey(mnemonic);
+    const params = await this.#getMinParams();
+    const txn = algosdk.makeApplicationOptInTxn(account.addr, params, appId);
+    console.log("Prepare Betting complete!");
+    return this.#sendSingleTxn(account.sk, txn);
+  }
+
+  /**
+   * @param {string} mnemonic
+   * @param {number} appId
+   * @param {number} amount
+   * @param {string} myTeam
+   * @param {string} escrowAddr
+   */
+  async userBet(mnemonic, appId, amount, myTeam, escrowAddr) {
+    console.log("Betting starting...");
+    const account = algosdk.mnemonicToSecretKey(mnemonic);
+    console.log(`    ${account.addr} is betting on ${myTeam} ${amount} USDC`);
+    const params = await this.#getMinParams();
+    const txn0 = await this.#makeUsdcTransferTxn(account.addr, escrowAddr, amount);
+    const txn1 = algosdk.makeApplicationNoOpTxn(account.addr, params, appId, [stringToByteArray('bet'), stringToByteArray(myTeam)]);
+    await this.#sendDoubleTxns(account.sk, txn0, account.sk, txn1);
+    console.log("Betting complete!");
+  }
+
+  /**
+   * Claim winnings for a given user.
+   * @param {string} mnemonic
+   * @param {number} appId
+   * @param {string} escrowAddr
+   * @param {string} escrowSk
+   * @param {number} myBet
+   * @param {number} myTeamTotal
+   * @param {number} otherTeamTotal
+   */
+   async userClaim(mnemonic, appId, escrowAddr, escrowSk, myBet, myTeamTotal, otherTeamTotal) {
+    const account = algosdk.mnemonicToSecretKey(mnemonic);
+    const amount = this.#calculateClaimAmount(myBet, myTeamTotal, otherTeamTotal);
+    console.log("Claiming " + amount + " with account " + account.addr);
+    await this.getAccountAssetInfo(escrowAddr, this.usdcAssetId);
+    await this.getAccountAssetInfo(account.addr, this.usdcAssetId);
+    const params = await this.#getMinParams();
+    const txn0 = await this.#makeUsdcTransferTxn(escrowAddr, account.addr, amount);
+    const txn1 = algosdk.makeApplicationNoOpTxn(account.addr, params, appId, [stringToByteArray('claim')]);
+    await this.#sendDoubleTxns(escrowSk, txn0, account.sk, txn1);
+    await this.getAccountAssetInfo(escrowAddr, this.usdcAssetId);
+    await this.getAccountAssetInfo(account.addr, this.usdcAssetId);
+    console.log("Claim complete!");
+  }
+
+  /**
+   * Helper function used to calculate how much a user is entitled to if they win for the given parameters.
+   * @param {number} myBet 
+   * @param {number} myTeamTotal 
+   * @param {number} otherTeamTotal 
+   * @param {number} fee 
+   * @returns {number} The amount a user may claim.
+   */
+  #calculateClaimAmount(myBet, myTeamTotal, otherTeamTotal) {
+    return Math.floor(myBet / myTeamTotal * (myTeamTotal + otherTeamTotal) - this.fixedFee)
+  }
+
+  /**
+   * Query the blockchain for suggested params, and set flat fee to True and the fee to the minimum.
+   */
+  async #getMinParams() {
+    const suggestedParams = await AlgoSigner.algod({
+      ledger: this.ledgerName,
+      path: '/v2/transactions/params'
+    });
+  
+    suggestedParams.flatFee  = true;
+    suggestedParams.fee = algosdk.ALGORAND_MIN_TX_FEE;
+  
+    return suggestedParams
+  }
+
+  /**
+   * @param {Uint8Array} sk 
+   * @param {algosdk.Transaction} txn 
+   */
+  async #sendSingleTxn(sk, txn) {
+    const signedTxn = txn.signTxn(sk);
+    const base64Txn = AlgoSigner.encoding.msgpackToBase64(signedTxn);
+    await AlgoSigner.send({
+      ledger: this.ledgerName,
+      tx: base64Txn,
+    });
+  }
+
+  /**
+   * @param {Uint8Array} sk0
+   * @param {algosdk.Transaction} txn0
+   * @param {Uint8Array} sk1
+   * @param {algosdk.Transaction} txn1
+   */
+  async #sendDoubleTxns(sk0, txn0, sk1, txn1) {
+    const gid = algosdk.computeGroupID([txn0, txn1]);
+    txn0.group = gid;
+    txn1.group = gid;
+    const signedTxn0 = txn0.signTxn(sk0);
+    const signedTxn1 = txn1.signTxn(sk1);
+    let combinedBinaryTxns = new Uint8Array(signedTxn0.byteLength + signedTxn1.byteLength);
+    combinedBinaryTxns.set(signedTxn0, 0);
+    combinedBinaryTxns.set(signedTxn1, signedTxn0.byteLength);
+    let combinedBase64Txns = AlgoSigner.encoding.msgpackToBase64(combinedBinaryTxns);
+    await AlgoSigner.send({
+      ledger: this.ledgerName,
+      tx: combinedBase64Txns,
+    });
+  }
+
+  /**
+   * @param {string} from 
+   * @param {string} to 
+   * @param {number} amount 
+   * @param {boolean} closeAtFrom
+   * @returns 
+   */
+  async #makeUsdcTransferTxn(from, to, amount, closeAtFrom=false) {
+  return algosdk.makeAssetTransferTxnWithSuggestedParams(
+    from,
+    to,
+    closeAtFrom ? from : undefined,
+    undefined,
+    amount,
+    undefined,
+    this.usdcAssetId,
+    await this.#getMinParams())
+  }
+}
+
 
 module.exports = {
   AutobookieCore,
   AutobookieDapp,
+  AlgoSignerWrapper,
   USDC_ASSET_ID_TESTNET,
   USDC_ASSET_ID_MAINNET,
 }
